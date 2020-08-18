436b3bb7726bf1deb5daeef86d4728091312dc7c
gaoljhy <gjlove666@hotmail.com>
2020-08-18 01:41:44 +0000

    cap3

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,24 +1,23 @@
+/**
+ * Copyright © 2020 Mr.G. All rights reserved.
+ *
+ * @author: Mr.G
+ * @date: 2020-08-17
+ * @TODO : {
+	 1. 类型转换
+	 :TODO}
+*/
 package main
 
 import (
 	"fmt"
+	"strconv"
 )
-
-//  重新定义一个 type
-type num int
-
-const (
-	one num = iota
-	two
-	three
-	four
-	five
-)
-const ten int32 = 10
 
 func main() {
-
-	fmt.Println(one)
-	fmt.Println(ten)
+	str := "123455"
+	strtoi, _ := strconv.Atoi(str)
+	strtoi++
+	fmt.Printf("type %T value%n \n", str, strtoi)
 
 }
